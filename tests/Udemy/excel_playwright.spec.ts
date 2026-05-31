e34a3f5384277b77e69b8c8de6e59bf94738856b
Rabi Ranjan Kumar <rbranjan47@gmail.com>
2026-06-01 01:43:40 +0530

    download_excel

--- tests/Udemy/excel_playwright.spec.ts
+++ tests/Udemy/excel_playwright.spec.ts
@@ -1,11 +1,11 @@
-import ExcelJS from 'exceljs'
-
+import ExcelJS from 'exceljs';
+import * as fs from 'fs';
 
 async function readExcel() {
     const workbook = new ExcelJS.Workbook();
     await workbook.xlsx.readFile("download.xlsx");
-    const worksheet = workbook.getWorksheet("Sheet1");
 
+    const worksheet = workbook.getWorksheet("Sheet1");
     if (!worksheet) {
         throw new Error('Worksheet "Sheet1" not found in download.xlsx');
     }
@@ -15,8 +15,36 @@ async function readExcel() {
             console.log(
                 'Row ' + rowNumber + ' cell ' + cellNumber + ' = ' + cell.value
             );
+
+            if (cell.value === 'Apple') {
+                console.log('Apple Present');
+                console.log('Row Number is :' + rowNumber);
+                console.log('Column Number is :' + cellNumber);
+            }
         });
-    })
+    });
 }
 
-readExcel();
+async function writeExcel() {
+    const workbook = new ExcelJS.Workbook();
+    await workbook.xlsx.readFile("download.xlsx");          
+
+    const worksheet = workbook.getWorksheet("Sheet1");
+    if (!worksheet) {
+        throw new Error('Worksheet "Sheet1" not found in download.xlsx');
+    }
+
+    const cell = worksheet.getCell(3, 2);
+    cell.value = "Strawberry";
+
+    await workbook.xlsx.writeFile("download_temp.xlsx");   
+    fs.renameSync("download_temp.xlsx", "download.xlsx");   
+    console.log("Write successful");
+}
+
+async function main() {
+    await writeExcel();
+    await readExcel();
+}
+
+main();
